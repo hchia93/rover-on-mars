4e08b33d3d31a9f0a7c0a2e6b4e3f37905fec2a0
Hyrex Chia JianFei <10288953+hchia93@users.noreply.github.com>
2025-05-28 20:31:53 +0800

    Created project's solution, updated license and preview

--- RoverOnMars/mars.cpp
+++ RoverOnMars/mars.cpp
@@ -10,24 +10,24 @@
 
 void Mars::init()
 {
-    char objects[] = {' ', ' ', ' ', ' ', ' ', ' ', 'X', '#', '@', '$' };
-    int numOfObjects = (sizeof(objects)/sizeof(*objects)); 
+    char objects[] = { ' ', ' ', ' ', ' ', ' ', ' ', 'X', '#', '@', '$' };
+    int numOfObjects = (sizeof(objects) / sizeof(*objects));
 
     // TODO : Size to be random generated, perhaps multiplication of 5
     m_DimX = 15;
     m_DimY = 15;
 
     // Allocation
-    map.resize(m_DimY); 
-    for(int i = 0 ; i < m_DimY ; ++i)
+    map.resize(m_DimY);
+    for (int i = 0; i < m_DimY; ++i)
     {
         map[i].resize(m_DimX); //resize each rows
     }
 
     // Map object initialization
-    for (int i = 0; i <m_DimY; ++i)
+    for (int i = 0; i < m_DimY; ++i)
     {
-        for (int j = 0 ; j < m_DimX; ++j)
+        for (int j = 0; j < m_DimX; ++j)
         {
             int objectID = std::rand() % numOfObjects;
             map[i][j] = objects[objectID];
@@ -37,11 +37,11 @@ void Mars::init()
 
 void Mars::display()
 {
-    if(m_GameOver)
+    if (m_GameOver)
     {
         system("cls");
         std::cout << "Game Over" << std::endl;
-        std::cout<<"Gold Count: " <<m_DisplayScore << std::endl;
+        std::cout << "Gold Count: " << m_DisplayScore << std::endl;
         return;
     }
 
@@ -50,15 +50,15 @@ void Mars::display()
     std::cout << " -------------------------------" << std::endl;
     std::cout << " = Curiosity, Welcome to Mars! =" << std::endl;
     std::cout << " -------------------------------" << std::endl;
-    for (int i = 0 ; i < m_DimY; ++i)
+    for (int i = 0; i < m_DimY; ++i)
     {
         std::cout << "  "; drawSeparator();
 
         // Row Index Handling
         // Fill in a space for digit less than 10 that causes alignment on rows.
         const int rowIndex = m_DimY - i;
-        if(rowIndex < 10) 
-            std::cout<< " ";
+        if (rowIndex < 10)
+            std::cout << " ";
         std::cout << std::setw(1) << rowIndex;
 
         drawRow(i);
@@ -67,14 +67,14 @@ void Mars::display()
     // Reserve space for index then start draw boarder
     std::cout << "  "; drawSeparator();
 
-     // Column Index Handling - Tenth digit
+    // Column Index Handling - Tenth digit
     std::cout << "  ";
     for (int j = 0; j < m_DimX; ++j)
     {
         int digit = (j + 1) / 10;
         std::cout << " ";
 
-        if(digit == 0)
+        if (digit == 0)
         {
             std::cout << " ";
         }
@@ -87,17 +87,17 @@ void Mars::display()
 
     // Column Index Handling - Single digit
     std::cout << "  ";
-    for (int j=0; j< m_DimX; ++j)
+    for (int j = 0; j < m_DimX; ++j)
     {
-        std::cout << " " << (j + 1) %10;
+        std::cout << " " << (j + 1) % 10;
     }
     std::cout << std::endl << std::endl;
 
-    std::cout<<std::endl;
-    std::cout<<"Gold Count: " <<m_DisplayScore << std::endl;
+    std::cout << std::endl;
+    std::cout << "Gold Count: " << m_DisplayScore << std::endl;
 }
 
-char Mars::getObject(const Point& point) const 
+char Mars::getObject(const Point& point) const
 {
     return map[point.y][point.x];
 }
@@ -151,20 +151,20 @@ void Mars::drawRow(const int i)
 {
     for (int j = 0; j < m_DimX; ++j)
     {
-        char display = '?'; 
-        
+        char display = ' ';
+
         Point p = Point(j, i);
         const bool isPlayer = (m_RoverLocation == p);
-        const bool isFrontRow = matchPoints(p, EnumUtil::getPointRowAheadOf(m_RoverLocation, m_RoverFacing)); 
+        const bool isFrontRow = matchPoints(p, EnumUtil::getPointRowAheadOf(m_RoverLocation, m_RoverFacing));
         if (isPlayer)
         {
             display = EnumUtil::toSymbol(m_RoverFacing);
         }
-        else if(isExplored(p) || m_IsDebug) 
+        else if (isExplored(p) || m_IsDebug)
         {
             display = map[i][j];
         }
-        
+
         std::cout << "|" << display;
     }
     std::cout << "|" << std::endl;
@@ -172,9 +172,9 @@ void Mars::drawRow(const int i)
 
 bool Mars::matchPoints(const Point& point, std::vector<Point> points)
 {
-    for(Point& p : points)
+    for (Point& p : points)
     {
-        if(p == point)
+        if (p == point)
         {
             return true;
         }
@@ -185,9 +185,9 @@ bool Mars::matchPoints(const Point& point, std::vector<Point> points)
 
 bool Mars::isExplored(const Point& point)
 {
-    for(auto&& p : *m_MapReference)
+    for (auto&& p : *m_MapReference)
     {
-        if(p == point)
+        if (p == point)
         {
             return true;
         }
--- RoverOnMars/rover.cpp
+++ RoverOnMars/rover.cpp
@@ -9,14 +9,14 @@ void Rover::land(Mars& mars)
 {
     m_MarsReference = &mars;
 
-    const int startingFacing =  std::rand() % 4;
+    const int startingFacing = std::rand() % 4;
     m_CurrentFacing = static_cast<EDirection>(startingFacing);
 
     m_Location.x = std::rand() % mars.getDimX() + 1;
     m_Location.y = std::rand() % mars.getDimY() + 1;
-    
+
     // Reroll until it is valid
-    while(!m_MarsReference->isInsideMap(m_Location))
+    while (!m_MarsReference->isInsideMap(m_Location))
     {
         m_Location.x = std::rand() % mars.getDimX() + 1;
         m_Location.y = std::rand() % mars.getDimY() + 1;
@@ -24,7 +24,7 @@ void Rover::land(Mars& mars)
 
     m_MarsReference->setRoverInfo(m_Location, m_CurrentFacing);
     m_MarsReference->setMap(&m_Map);
-   
+
     m_Map.push_back(m_Location);
     explore();
 }
@@ -34,7 +34,7 @@ void Rover::turnLeft()
     // This function is relative to self-orientation
     EDirection newFacing = EnumUtil::getLeftOf(m_CurrentFacing);
     m_CurrentFacing = newFacing;
-    
+
     m_MarsReference->setRoverInfo(m_Location, newFacing);
     explore();
 }
@@ -53,36 +53,36 @@ void Rover::move()
 {
     // Peek ahead , do not mark explored yet. Current location should already mark explored.
     std::vector<Point> points = EnumUtil::getPointRowAheadOf(m_Location, m_CurrentFacing);
-    
-    if(m_MarsReference->isInsideMap(points[0]))
+
+    if (m_MarsReference->isInsideMap(points[0]))
     {
-        if(m_MarsReference->isHill(points[0]))
+        if (m_MarsReference->isHill(points[0]))
         {
             // Do not move if it is hill ahead.
             return;
         }
-       
+
         // Commit movement
         m_Location = points[0];
         m_MarsReference->setRoverInfo(points[0], m_CurrentFacing);
 
         // Post movement handling
-        if(!m_MarsReference->isEmpty(points[0])) 
+        if (!m_MarsReference->isEmpty(points[0]))
         {
-            if(m_MarsReference->isGold(points[0]))
+            if (m_MarsReference->isGold(points[0]))
             {
                 m_GoldScore++;
                 m_MarsReference->setObject(points[0], ' ');
             }
 
-            if(m_MarsReference->isTrap(points[0]))
+            if (m_MarsReference->isTrap(points[0]))
             {
                 m_MarsReference->setGameOver();
             }
         }
-        
+
         m_MarsReference->setDisplayScore(m_GoldScore);
-        
+
         explore();
     }
 }
@@ -91,19 +91,19 @@ void Rover::processInput(char c)
 {
     m_MarsReference->setDebug(false);
 
-    if(c == 'a' || c == 'A' )
+    if (c == 'a' || c == 'A')
     {
         turnLeft();
     }
-    else if(c == 'w' || c == 'W' )
+    else if (c == 'w' || c == 'W')
     {
         move();
     }
-    else if(c == 'd' || c == 'D' )
+    else if (c == 'd' || c == 'D')
     {
         turnRight();
     }
-    else if(c == 't' || c == 'T' )
+    else if (c == 't' || c == 'T')
     {
         m_MarsReference->setDebug(true);
     }
@@ -112,9 +112,9 @@ void Rover::processInput(char c)
 void Rover::explore()
 {
     // Explore the next direction row.
-    for(const Point& p : EnumUtil::getPointRowAheadOf(m_Location, m_CurrentFacing))
+    for (const Point& p : EnumUtil::getPointRowAheadOf(m_Location, m_CurrentFacing))
     {
-        if(!m_MarsReference->isExplored(p))
+        if (!m_MarsReference->isExplored(p))
         {
             m_Map.push_back(p);
         }
